515cc92632c6d87bc6f8f600f09e4b806f8524d5
Jeff Burdges <burdges@gnunet.org>
2019-02-28 14:39:32 +0100

    Try this

--- docs/mathjax-config.js
+++ docs/mathjax-config.js
@@ -3,7 +3,7 @@ MathJax.Hub.Config({
     extensions: ["tex2jax.js"],
     jax: ["input/TeX", "output/HTML-CSS"],
     tex2jax: {
-      inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+      inlineMath: [ ['$','$'] ],
       displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
       processEscapes: true
     },
