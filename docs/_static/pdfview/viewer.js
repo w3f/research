0f35b99e96b96ebcadea3113d5d01acf4d516682
Florian Franzen <Florian.Franzen@gmail.com>
2021-01-19 14:58:53 +0100

    pdf.js: change mozilla to w3f pages subdomain

--- docs/_static/pdfview/viewer.js
+++ docs/_static/pdfview/viewer.js
@@ -2161,7 +2161,7 @@ var PDFViewerApplication = {
 exports.PDFViewerApplication = PDFViewerApplication;
 var validateFileURL;
 {
-  var HOSTED_VIEWER_ORIGINS = ["null", "http://mozilla.github.io", "https://mozilla.github.io"];
+  var HOSTED_VIEWER_ORIGINS = ["null", "http://w3f.github.io", "https://w3f.github.io"];
 
   validateFileURL = function validateFileURL(file) {
     if (file === undefined) {
