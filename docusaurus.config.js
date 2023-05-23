b3d8a769c61d7e1f81f20e59a2d2f86828a80b67
Matteo Casonato <matteocasonato@protonmail.com>
2023-05-23 11:15:52 +0200

    Fix edit button

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,7 +47,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/w3f/research/blob/main/docs',
+            'https://github.com/w3f/research/blob/master',
           routeBasePath: '/',
         },
         theme: {
